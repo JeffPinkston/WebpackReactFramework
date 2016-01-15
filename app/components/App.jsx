5f1097da19d4e1b44cdae765fb64b62aa1a91120
Jeff Pinkston <jeff.pinkston@pearson.com>
2016-01-15 10:34:34 -0600

    Created empty App.jsx

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,33 +1,13 @@
-import AltContainer from 'alt-container';
 import React from 'react';
-import Lanes from './Lanes.jsx';
-import LaneStore from '../stores/LaneStore';
-import LaneActions from '../actions/LaneActions';
-import {DragDropContext} from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
 
-
-@DragDropContext(HTML5Backend)
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <button className="add-lane" onClick={this.addLane}>+</button>
-        <AltContainer
-                stores={[LaneStore]}
-                inject={{
-                  lanes: () => LaneStore.getState().lanes || []
-                }}
-              >
-                <Lanes/>
-            </AltContainer>
-      </div>
-    );
-  }
-  addLane() {
-    LaneActions.create({name: 'New lane'});
-  }
-  responseHandler() {
-    console.log('Ajax Response');
-  }
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = 'App';
+    }
+    render() {
+        return <div>App</div>;
+    }
 }
+
+export default App;
